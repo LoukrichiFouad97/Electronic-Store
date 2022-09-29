710c5e8997f35dcb77165dfbc6fccea7da276b51
loukrichiFouad97 <loukrichi21@gmail.com>
2022-09-29 11:20:59 +0100

    added styles and responsiveness to the special oppertunity section.

--- src/components/SpecialOppertunity/index.js
+++ src/components/SpecialOppertunity/index.js
@@ -1,9 +1,29 @@
-import { Container } from "react-bootstrap";
+import { Container, Row, Col } from "react-bootstrap";
+
+import "./style.scss";
 
 function SpecialOppertunity() {
   return (
-    <Container>
-      <h2>SpecialOppertunity</h2>
+    <Container className="special-oppertunity-container">
+      <Row className="special-oppertunity">
+        <Col md="12" lg="6" className="special-oppertunity-head">
+          <h4>
+            shop and <span>save big on hottest</span> products
+          </h4>
+        </Col>
+        <Col md="12" lg="6" className="special-oppertunity-content">
+          <div className="special-oppertunity-offer">
+            <p>
+              <strong>from </strong>
+              <span>$79.00</span>
+            </p>
+            <p>Don't miss this special opportunity today.</p>
+          </div>
+          <button className="special-oppertunity-btn btn btn-primary">
+            Shop Now
+          </button>
+        </Col>
+      </Row>
     </Container>
   );
 }
--- src/components/TrendingProducts/index.js
+++ src/components/TrendingProducts/index.js
@@ -1,18 +1,287 @@
 import { Container, Row, Col } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import { HiOutlineArrowNarrowRight } from "react-icons/hi";
+import { AiFillStar, AiOutlineStar } from "react-icons/ai";
+
+import productLogo from "../../assets/air3-400x400.jpg";
+import "./style.scss";
 
 function TrendingProducts() {
   return (
-    <Container>
-      <h2>Trending Products</h2>
-      <Row>
-        <Col>1</Col>
-        <Col>2</Col>
-        <Col>3</Col>
+    <Container className="trending-products">
+      <Row className="trending-products-head">
+        <Col>
+          <h2>Trending Products</h2>
+        </Col>
+        <Col className="d-flex justify-content-end align-items-end trending-products-head-link">
+          <Link className="fw-bold">
+            View All
+            <HiOutlineArrowNarrowRight className="ms-2" />
+          </Link>
+        </Col>
       </Row>
-      <Row>
-        <Col>1</Col>
-        <Col>2</Col>
-        <Col>3</Col>
+      <Row className="trending-products-content">
+        <Row className="product-wrapper">
+          <Col lg="6" xl="4" className="product-wrapper-col">
+            <div className="product-thumbnail">
+              <div className="product-badges"></div>
+              <Link>
+                <img src={productLogo} alt="" width="140px" />
+              </Link>
+            </div>
+            <div className="product-content">
+              <h3 className="product-title">
+                <Link to="/shop" className="product-title-link">
+                  Apple 10.9-inch iPad Air Wi-Fi Cellular 64GB
+                </Link>
+              </h3>
+              <div className="product-rating">
+                <span className="rating-icon">
+                  <AiFillStar />
+                </span>
+                <span className="rating-icon">
+                  <AiFillStar />
+                </span>
+                <span className="rating-icon">
+                  <AiFillStar />
+                </span>
+                <span className="rating-icon">
+                  <AiFillStar />
+                </span>
+                <span className="rating-icon">
+                  <AiOutlineStar />
+                </span>
+              </div>
+              <div className="product-price">
+                <ins>$629.29</ins>
+                <del>$699</del>
+              </div>
+              <div className="product-details">
+                <ul>
+                  <li class="SpecHighlights-list-label">Screen Size 10.9 in</li>
+                  <li>Operating System Apple iOS</li>
+                  <li>Product Length 9.74 in</li>
+                </ul>
+              </div>
+            </div>
+          </Col>
+          <Col lg="6" xl="4" className="product-wrapper-col">
+            <div className="product-thumbnail">
+              <div className="product-badges"></div>
+              <Link>
+                <img src={productLogo} alt="" width="140px" />
+              </Link>
+            </div>
+            <div className="product-content">
+              <h3 className="product-title">
+                <Link to="/shop" className="product-title-link">
+                  Apple 10.9-inch iPad Air Wi-Fi Cellular 64GB
+                </Link>
+              </h3>
+              <div className="product-rating">
+                <span className="rating-icon">
+                  <AiFillStar />
+                </span>
+                <span className="rating-icon">
+                  <AiFillStar />
+                </span>
+                <span className="rating-icon">
+                  <AiFillStar />
+                </span>
+                <span className="rating-icon">
+                  <AiFillStar />
+                </span>
+                <span className="rating-icon">
+                  <AiOutlineStar />
+                </span>
+              </div>
+              <div className="product-price">
+                <ins>$629.29</ins>
+                <del>$699</del>
+              </div>
+              <div className="product-details">
+                <ul>
+                  <li class="SpecHighlights-list-label">Screen Size 10.9 in</li>
+                  <li>Operating System Apple iOS</li>
+                  <li>Product Length 9.74 in</li>
+                </ul>
+              </div>
+            </div>
+          </Col>
+          <Col lg="6" xl="4" className="product-wrapper-col">
+            <div className="product-thumbnail">
+              <div className="product-badges"></div>
+              <Link>
+                <img src={productLogo} alt="" width="140px" />
+              </Link>
+            </div>
+            <div className="product-content">
+              <h3 className="product-title">
+                <Link to="/shop" className="product-title-link">
+                  Apple 10.9-inch iPad Air Wi-Fi Cellular 64GB
+                </Link>
+              </h3>
+              <div className="product-rating">
+                <span className="rating-icon">
+                  <AiFillStar />
+                </span>
+                <span className="rating-icon">
+                  <AiFillStar />
+                </span>
+                <span className="rating-icon">
+                  <AiFillStar />
+                </span>
+                <span className="rating-icon">
+                  <AiFillStar />
+                </span>
+                <span className="rating-icon">
+                  <AiOutlineStar />
+                </span>
+              </div>
+              <div className="product-price">
+                <ins>$629.29</ins>
+                <del>$699</del>
+              </div>
+              <div className="product-details">
+                <ul>
+                  <li class="SpecHighlights-list-label">Screen Size 10.9 in</li>
+                  <li>Operating System Apple iOS</li>
+                  <li>Product Length 9.74 in</li>
+                </ul>
+              </div>
+            </div>
+          </Col>
+          <Col lg="6" xl="4" className="product-wrapper-col">
+            <div className="product-thumbnail">
+              <div className="product-badges"></div>
+              <Link>
+                <img src={productLogo} alt="" width="140px" />
+              </Link>
+            </div>
+            <div className="product-content">
+              <h3 className="product-title">
+                <Link to="/shop" className="product-title-link">
+                  Apple 10.9-inch iPad Air Wi-Fi Cellular 64GB
+                </Link>
+              </h3>
+              <div className="product-rating">
+                <span className="rating-icon">
+                  <AiFillStar />
+                </span>
+                <span className="rating-icon">
+                  <AiFillStar />
+                </span>
+                <span className="rating-icon">
+                  <AiFillStar />
+                </span>
+                <span className="rating-icon">
+                  <AiFillStar />
+                </span>
+                <span className="rating-icon">
+                  <AiOutlineStar />
+                </span>
+              </div>
+              <div className="product-price">
+                <ins>$629.29</ins>
+                <del>$699</del>
+              </div>
+              <div className="product-details">
+                <ul>
+                  <li class="SpecHighlights-list-label">Screen Size 10.9 in</li>
+                  <li>Operating System Apple iOS</li>
+                  <li>Product Length 9.74 in</li>
+                </ul>
+              </div>
+            </div>
+          </Col>
+          <Col lg="6" xl="4" className="product-wrapper-col">
+            <div className="product-thumbnail">
+              <div className="product-badges"></div>
+              <Link>
+                <img src={productLogo} alt="" width="140px" />
+              </Link>
+            </div>
+            <div className="product-content">
+              <h3 className="product-title">
+                <Link to="/shop" className="product-title-link">
+                  Apple 10.9-inch iPad Air Wi-Fi Cellular 64GB
+                </Link>
+              </h3>
+              <div className="product-rating">
+                <span className="rating-icon">
+                  <AiFillStar />
+                </span>
+                <span className="rating-icon">
+                  <AiFillStar />
+                </span>
+                <span className="rating-icon">
+                  <AiFillStar />
+                </span>
+                <span className="rating-icon">
+                  <AiFillStar />
+                </span>
+                <span className="rating-icon">
+                  <AiOutlineStar />
+                </span>
+              </div>
+              <div className="product-price">
+                <ins>$629.29</ins>
+                <del>$699</del>
+              </div>
+              <div className="product-details">
+                <ul>
+                  <li class="SpecHighlights-list-label">Screen Size 10.9 in</li>
+                  <li>Operating System Apple iOS</li>
+                  <li>Product Length 9.74 in</li>
+                </ul>
+              </div>
+            </div>
+          </Col>
+          <Col lg="6" xl="4" className="product-wrapper-col">
+            <div className="product-thumbnail">
+              <div className="product-badges"></div>
+              <Link>
+                <img src={productLogo} alt="" width="140px" />
+              </Link>
+            </div>
+            <div className="product-content">
+              <h3 className="product-title">
+                <Link to="/shop" className="product-title-link">
+                  Apple 10.9-inch iPad Air Wi-Fi Cellular 64GB
+                </Link>
+              </h3>
+              <div className="product-rating">
+                <span className="rating-icon">
+                  <AiFillStar />
+                </span>
+                <span className="rating-icon">
+                  <AiFillStar />
+                </span>
+                <span className="rating-icon">
+                  <AiFillStar />
+                </span>
+                <span className="rating-icon">
+                  <AiFillStar />
+                </span>
+                <span className="rating-icon">
+                  <AiOutlineStar />
+                </span>
+              </div>
+              <div className="product-price">
+                <ins>$629.29</ins>
+                <del>$699</del>
+              </div>
+              <div className="product-details">
+                <ul>
+                  <li class="SpecHighlights-list-label">Screen Size 10.9 in</li>
+                  <li>Operating System Apple iOS</li>
+                  <li>Product Length 9.74 in</li>
+                </ul>
+              </div>
+            </div>
+          </Col>
+        </Row>
+        <Row></Row>
       </Row>
     </Container>
   );
